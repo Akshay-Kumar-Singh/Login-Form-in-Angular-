bf96a5be6e5937da7961ec999acceabfb341003c
Abhishek Rana <abhishek_rana@live.com>
2018-07-31 07:52:54 +0530

    2nd commit

--- app.component.ts
+++ app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -7,13 +6,5 @@ import { Router } from '@angular/router'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-
-  samplevariable;
-constructor(private router : Router){}
-onButtonClick(){
-  localStorage.setItem('key', this.samplevariable);
-  this.router.navigate(['/sample'])
-
-}
+  title = 'Login';
 }
--- app.module.ts
+++ app.module.ts
@@ -1,32 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SampletwoComponent } from './sampletwo/sampletwo.component';
-
-import { RouterModule, Routes  } from '@angular/router'
-
-const routes : Routes = [{
-  path : 'sample',
-  component : SampletwoComponent
-}]
+import { LoginformComponent } from './loginform/loginform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampletwoComponent
+    LoginformComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  
-}
+export class AppModule { }
